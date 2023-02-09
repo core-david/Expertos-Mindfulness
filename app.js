5f3483c86ed8843345d3e3ebe010710398b89933
core-david <davale1201@gmail.com>
2023-02-09 17:45:03 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+
+app.listen(3000);
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+    res.render('index')
+});
+
+app.get('/proceso', (req, res) => {
+    res.render('proceso')
+});
+
+app.get('/cursos/estres', (req, res) =>{
+    res.render('cursos/estres')
+});
+
+app.get('/cursos/liderazgo', (req, res) =>{
+    res.render('cursos/liderazgo')
+});
+
+app.get('/cursos/resiliencia', (req, res) =>{
+    res.render('cursos/resiliencia')
+});
+
+app.get('/cursos/ie', (req, res) =>{
+    res.render('cursos/ie')
+});
+
+app.get('/cursos/comunicacion', (req, res) =>{
+    res.render('cursos/comunicacion')
+});
